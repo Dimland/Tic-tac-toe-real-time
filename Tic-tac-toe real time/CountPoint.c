3d8fee48f9acbcae4389e86d1e44e2cf8e68692e
Dimland <Dimaq21@gmail.com>
2022-10-15 22:08:57 +0300

    Добавьте файлы проекта.

---
+++ Tic-tac-toe real time/CountPoint.c
@@ -0,0 +1,9 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <windows.h>
+#include <new.h>
+#include <stdio.h>
+#include <stdio.h >
+#include <conio.h> 
+#include <winuser.h>
+#include <locale.h>
+#include <wchar.h>
